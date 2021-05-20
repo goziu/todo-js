40bb4dbd229cb0785dae608293c8506cc9d55e1a
goziu <84424993+goziu@users.noreply.github.com>
2021-05-20 14:12:10 +0900

    素のjsによる完成

--- src/index.js
+++ src/index.js
@@ -4,13 +4,22 @@ const onClickAdd = () => {
   // テキストボックスの値を取得して初期化
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
+  createIncompleteList(inputText);
+};
 
+// 未完了リストから指定の要素を削除
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplete-list").removeChild(target);
+};
+
+// 未完了リストに追加する関数
+const createIncompleteList = (text) => {
   // div 生成
   const div = document.createElement("div");
   div.className = "list-row";
   // li 生成
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
 
   // button（完了）タグ生成
   const completeButton = document.createElement("button");
@@ -39,7 +48,7 @@ const onClickAdd = () => {
 
       // テキスト取得
       const text = backButton.parentNode.firstElementChild.innerText;
-      console.log(text);
+      createIncompleteList(text);
     });
 
     // divタグの子要素に各要素を設定
@@ -65,12 +74,6 @@ const onClickAdd = () => {
 
   // 未完了リストに追加
   document.getElementById("incomplete-list").appendChild(div);
-  //console.log(div);
-};
-
-// 未完了リストから指定の要素を削除
-const deleteFromIncompleteList = (target) => {
-  document.getElementById("incomplete-list").removeChild(target);
 };
 
 document
